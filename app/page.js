08fd6a03e8f713a93ff4114c1d9114af67129fca
Deepak Kumar Yadav <dky422003@gmail.comm>
2024-12-27 00:12:16 +0530

    completed setting of user authentication

--- app/page.js
+++ app/page.js
@@ -2,7 +2,7 @@ import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <div>
+    <div className="mt-40">
       <Button variant="destructive">Hello</Button>
     </div>
   );
